13224d4d81c31b76364a0bcaa84012c3e3723391
pc <djebritichrak80@gmail.com>
2020-08-25 16:10:27 +0100

    final

--- app/src/main/java/com/example/myapplication/SearchProductsActivity.java
+++ app/src/main/java/com/example/myapplication/SearchProductsActivity.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -51,6 +52,10 @@ public class SearchProductsActivity extends AppCompatActivity
                 onStart();
             }
         });
+    }
+    public void onCustom(View view) {
+        Toast.makeText( this , "Produit favoris" , Toast.LENGTH_SHORT ).show();
+
     }
 
 
